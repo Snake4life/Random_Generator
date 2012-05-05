17e6d36f54da8bf62a5c1e71b735ed3a7bd8ff53
Abishek Muthian <developerdragon@gmail.com>
2012-05-05 22:33:35 +0530

    Added random generator demo
    Added the random generator Facebook application demo

---
+++ randomgenerator/generate.php
@@ -0,0 +1,177 @@
+<title><?=FB_CONFIG_APP_NAME?></title>
+<?php
+/* Copyright indiandragon, do not redistribute this code without the permission*/
+
+include 'facebook-php-sdk/src/facebook.php';
+include 'header.php'; 
+?>    
+
+        <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script> 
+
+        <script type="text/javascript" src="<?=html_root('scripts/jquery.toastmessage.js')?>"></script>
+        
+        
+        <link type="text/css" href="<?=html_root('scripts/css/jquery.toastmessage.css')?>" rel="stylesheet"/>
+        <style> 
+            body {
+                background: #fff;
+                color: #333;
+                font: 11px verdana, arial, helvetica, sans-serif;
+                text-align:center;
+                
+            }
+            a:link, a:visited, a:hover {
+                color: #666;
+                font-weight: bold;
+                text-decoration: none;
+            }
+        </style>  
+        
+
+<script language="JavaScript">
+
+<!--
+var ry
+
+
+
+
+
+
+
+
+
+
+// Start user configuration 
+
+//specify random images below. You can have as many as you wish
+  
+  var myimages=new Array()
+  myimages[1]="http://fbstore.indiandragon.in/randomgenerator/image1.jpg"
+  myimages[2]="http://fbstore.indiandragon.in/randomgenerator/image2.jpg"
+  myimages[3]="http://fbstore.indiandragon.in/randomgenerator/image3.jpg"
+  myimages[4]="http://fbstore.indiandragon.in/randomgenerator/image4.jpg"
+  myimages[5]="http://fbstore.indiandragon.in/randomgenerator/image5.jpg"
+  myimages[6]="http://fbstore.indiandragon.in/randomgenerator/image6.jpg"
+
+// Caption for the images  
+  
+  var cap=new Array()
+  cap[1]='Uzumaki Naruto'
+  cap[2]='Haruno Sakura'
+  cap[3]='Hyuuga Neji'
+  cap[4]='Rock Lee'
+  cap[5]='Saabaku No Gaara'
+  cap[6]='Uchiha Sasuke'
+  
+// Dialogue for the image
+
+var desc=new Array()
+desc[1]='I never give up... I never go back on my word... thats the way of the ninja'
+desc[2]='All I\'ve been doing is watching you two from behind... now, get a good look at my back'
+desc[3]='Iam neji Hyuuga'
+desc[4]='A genius, huh? What does that mean?'
+desc[5]='I fight for my sake only and live to love no one but myself'
+desc[6]='I have long since closed my eyes... My only goal is in the darkness'
+
+// End of user configuration
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Do not touch this code unless you know what you are doing
+
+function random_imglink(){
+
+  ry=Math.floor(Math.random()*myimages.length)
+
+  if (ry==0){ry=1}
+ 
+     document.write('<div id="div'+ry+'" style="text-align:center"><img id="img'+ry+'" src="'+myimages[ry]+'" border=0><br /></div>')
+      document.write('<div id="div'+ry+'" style="text-align:center"><h1>'+cap[ry]+' says, '+desc[ry]+'!!</h1></div>');
+
+}
+
+  random_imglink()
+
+//-->
+
+
+  
+
+  
+
+//Publish to the wall
+
+function publishToWall() {
+ 
+FB.ui(
+   {
+     method: 'feed',
+     name: '<?=FB_CONFIG_BUTTON_NAME?>',
+     link: '<?=FB_CONFIG_ROOT?>',
+     picture: myimages[ry],
+     caption: cap[ry],
+     description: desc[ry],
+   },
+   function(response) {
+     if (response && response.post_id) {
+		       	                           $().toastmessage('showSuccessToast', "Post has been published.");
+     } else {
+		    	                           $().toastmessage('showErrorToast', "Post was not published.");
+     }
+   }
+ );
+
+}
+
+ 
+  
+</script>
+
+<center>
+<span style="float: left;"><fb:like href="<?=FB_CONFIG_ROOT?>" layout="standard" show_faces="false" width="400" action="like" font="segoe ui" colorscheme="light"  send="true"/></span>
+
+<h2><?=FB_CONGRATULATION_MESSAGE?></h2>
+
+
+<script src="http://connect.facebook.net/en_US/all.js"></script>
+<script>
+  FB.init({
+    status : true, // check login status
+    cookie : true, // enable cookies to allow the server to access the session
+    xfbml  : true  // parse XFBML
+  });
+</script>
+
+<div id="results_options">
+<span style="float right;">	
+	<a href="#" onclick="publishToWall();"><img src="<?=html_root('images/publish_to_wall.png')?>"/></a>
+	<a href="<?=html_root('generate.php?q=0')?>"><img src="<?=html_root('images/retake_quiz.png')?>"/></a>
+</span>
+</div>
+
+
+ </center>
+
+
+<?php include 'footer.php'; ?>
+<script>FB.CanvasClient.startTimerToSizeToContent()</script>
---
+++ randomgenerator/include/config.php
@@ -0,0 +1,37 @@
+<!-- Copyright indiandragon, do not redistribute this code without the permission -->
+<?php
+
+/* Give the path of your server(host) */
+define('HTML_ROOT',				'http://fbstore.indiandragon.in/randomgenerator/');
+
+/* Give the path of your application */
+define('FB_CONFIG_ROOT',		'http://apps.facebook.com/randomgeneratordemo/');
+
+/* Give the name of your application */
+define('FB_CONFIG_APP_NAME',	'Which Naruto character are you today?');
+
+/* Do not touch the parameter below */
+define('FB_CONFIG_NEXT',		'http://apps.facebook.com/randomgeneratordemo/generate.php?q=0');
+
+/* Do not touch the parameter below */
+define('FB_CONFIG_PERMS',		'');
+
+/* Give your application id */
+define('FB_CONFIG_APP_ID',		'506d9ada4fc4bf9e8c47ff78a5ee05d7');
+
+/* Give your application secret */
+define('FB_CONFIG_APP_SECRET',	'62fb165ee87886d55f880bf6c696d7c3');
+
+/* Give your logo image */
+define('FB_CONFIG_LOGO_IMAGE',	'http://fbstore.indiandragon.in/randomgenerator/logo.png');
+
+/* Give your main page intro */
+define('FB_MAIN_PAGE_INTRO',	'Greetings!  Welcome to Random generator demo -- This is a niche applcation using advanced algorithm to generate your favorite random character. In this demo we generate a random naruto character. When you are ready to continue,simply click the button below');
+
+/* Give the button name in the main page */
+define('FB_CONFIG_BUTTON_NAME',	'Naruto Mania');
+
+/* Give the final congratulation message */
+define('FB_CONGRATULATION_MESSAGE',	'Congratulations you got your naruto character!!');
+
+?>
